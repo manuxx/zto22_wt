2d4a164062c46d523fd8a546eca1aa33838158b3
Jakub Brzozowski <brzozowski.jakub.andrzej@gmail.com>
2022-11-08 09:17:46 +0100

    refactor

--- PetShop/Pet.cs
+++ PetShop/Pet.cs
@@ -16,7 +16,7 @@ namespace Training.DomainClasses
             if (ReferenceEquals(this, other)) return true;
             return this.name == other.name 
                    && this.yearOfBirth == other.yearOfBirth 
-                   && this.price == other.price 
+                   && this.price.Equals(other.price) 
                    && this.species == other.species;
         }
         
@@ -25,7 +25,7 @@ namespace Training.DomainClasses
             if (ReferenceEquals(null, obj)) return false;
             if (ReferenceEquals(this, obj)) return true;
             if (obj.GetType() != this.GetType()) return false;
-            Pet other = (Pet)obj;
+            var other = (Pet)obj;
             return this.Equals(other);
         }
     }
