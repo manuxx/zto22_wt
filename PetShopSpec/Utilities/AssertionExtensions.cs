4784d93d6d8ef1fcdc81a842810e67255dd048ac
manuxx <manus@pg.gda.pl>
2022-11-08 09:07:00 +0100

    proste rozwiazanie jest dobre ...bo proste

--- PetShopSpec/Utilities/AssertionExtensions.cs
+++ PetShopSpec/Utilities/AssertionExtensions.cs
@@ -10,15 +10,20 @@ namespace Training.Specificaton
     {
         public static void ShouldContainOnlyInOrder<TItem>(this IEnumerable<TItem> items, params TItem[] ordered_items)
         {
-            AssertionExtensions.ShouldContainOnlyInOrder<TItem>(items, (IEnumerable<TItem>)ordered_items);
+            AssertionExtensions.ShouldContainOnlyInOrder<TItem>(items, (IEnumerable<TItem>) ordered_items);
         }
 
-        public static void ShouldContainOnlyInOrder<TItem>(this IEnumerable<TItem> items, IEnumerable<TItem> ordered_items)
+        public static void ShouldContainOnlyInOrder<TItem>(this IEnumerable<TItem> items,
+            IEnumerable<TItem> ordered_items)
         {
             List<TItem> source = new List<TItem>(items);
-            if (!Enumerable.Any<TItem>(Enumerable.Where<TItem>(ordered_items, (Func<TItem, int, bool>)((ordered_element, index) => !source[index].Equals((object)ordered_element)))))
+            if (!Enumerable.Any<TItem>(Enumerable.Where<TItem>(ordered_items,
+                    (Func<TItem, int, bool>) ((ordered_element, index) =>
+                        !source[index].Equals((object) ordered_element)))))
                 return;
-            throw new SpecificationException(string.Format("The set of items should only contain the items in the order {0}\r\nbut it actually contains the items:{1}", ordered_items.EachToUsefulString(), items.EachToUsefulString()));
+            throw new SpecificationException(string.Format(
+                "The set of items should only contain the items in the order {0}\r\nbut it actually contains the items:{1}",
+                ordered_items.EachToUsefulString(), items.EachToUsefulString()));
         }
     }
 }
