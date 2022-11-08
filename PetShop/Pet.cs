ceaa4124df3679531b07df5816cf32707bd5b7d1
Jakub Brzozowski <brzozowski.jakub.andrzej@gmail.com>
2022-11-08 09:16:58 +0100

    Add second Equals

--- PetShop/Pet.cs
+++ PetShop/Pet.cs
@@ -10,18 +10,23 @@ namespace Training.DomainClasses
         public float price { get; set; }
         public Species species { get; set; }
 
-        public override bool Equals(object obj)
+        public bool Equals(Pet other)
         {
-            if (obj == null)
-                return false;
-            if (obj.GetType() != this.GetType())
-                return false;
-            Pet other = (Pet)obj;
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
             return this.name == other.name 
                    && this.yearOfBirth == other.yearOfBirth 
                    && this.price == other.price 
                    && this.species == other.species;
         }
         
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            Pet other = (Pet)obj;
+            return this.Equals(other);
+        }
     }
 }
