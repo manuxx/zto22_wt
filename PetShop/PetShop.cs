cc421ad524eb4039ee0e5f5b74522f3e0488f501
Aleksander Błaszkiewicz <aleksander.blaszkiewicz@ihsmarkit.com>
2022-11-15 09:15:26 +0100

    Add MatchingPets

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Training.DomainClasses
 {
@@ -45,6 +46,57 @@ namespace Training.DomainClasses
             var result = new List<Pet>(_petsInTheStore);
             result.Sort((p1,p2)=>p1.name.CompareTo(p2.name));
             return result;
+        }
+
+        private IEnumerable<Pet> MatchingPets(Func<Pet, bool> condition)
+        {
+            foreach (var pet in _petsInTheStore)
+            {
+                if (condition(pet))
+                {
+                    yield return pet;
+                }
+            }
+        }
+
+        public IEnumerable<Pet> AllMice()
+        {
+            return MatchingPets(pet => pet.species == Species.Mouse);
+        }
+
+        public IEnumerable<Pet> AllFemalePets()
+        {
+            return MatchingPets(pet => pet.sex == Sex.Female);
+        }
+
+        public IEnumerable<Pet> AllCatsOrDogs()
+        {
+            return MatchingPets(pet => pet.species == Species.Dog || pet.species == Species.Cat);
+        }
+
+        public IEnumerable<Pet> AllPetsButNotMice()
+        {
+            return MatchingPets(pet => pet.species != Species.Mouse);
+        }
+
+        public IEnumerable<Pet> AllPetsBornAfter2010()
+        {
+            return MatchingPets(pet => pet.yearOfBirth > 2010);
+        }
+
+        public IEnumerable<Pet> AllDogsBornAfter2010()
+        {
+            return MatchingPets(pet => pet.yearOfBirth > 2010 && pet.species == Species.Dog);
+        }
+
+        public IEnumerable<Pet> AllMaleDogs()
+        {
+            return MatchingPets(pet => pet.sex == Sex.Male && pet.species == Species.Dog);
+        }
+
+        public IEnumerable<Pet> AllPetsBornAfter2011OrRabbits()
+        {
+            return MatchingPets(pet => pet.yearOfBirth > 2011 || pet.species == Species.Rabbit);
         }
     }
 }
