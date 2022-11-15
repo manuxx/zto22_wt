f4bd0907c5f2dea08b20222961e91a9bf7686eed
Jakub Brzozowski <brzozowski.jakub.andrzej@gmail.com>
2022-11-15 09:37:26 +0100

    make Utils internal static

--- PetShop/Utils.cs
+++ PetShop/Utils.cs
@@ -2,9 +2,9 @@ using System.Collections.Generic;
 
 namespace Training.DomainClasses
 {
-    public class Utils
+    internal static class Utils
     {
-        public static IEnumerable<TItem> EachElement<TItem>(IEnumerable<TItem> collection)
+        public static IEnumerable<TItem> EachElement<TItem>(this IEnumerable<TItem> collection)
         {
             foreach (var item in collection)
             {
