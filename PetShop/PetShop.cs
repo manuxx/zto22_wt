9a84394736a6cf5783e4870b0dee61d737a68b32
michal.matejuk <michal.matejuk@ihsmarkit.com>
2022-11-15 09:10:37 +0100

    done

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -30,14 +30,7 @@ namespace Training.DomainClasses
 
         public IEnumerable<Pet> AllCats()
         {
-            foreach (var pet in _petsInTheStore)
-            {
-                if (pet.species == Species.Cat)
-                {
-                    yield return pet;
-                }
-            }
-
+            return PetsWhere(p => p.species == Species.Cat);
         }
 
         public IEnumerable<Pet> AllPetsSortedByName()
@@ -45,6 +38,57 @@ namespace Training.DomainClasses
             var result = new List<Pet>(_petsInTheStore);
             result.Sort((p1,p2)=>p1.name.CompareTo(p2.name));
             return result;
+        }
+
+        public IEnumerable<Pet> AllMice()
+        {
+            return PetsWhere(p => p.species == Species.Mouse);
+        }
+
+        public IEnumerable<Pet> AllFemalePets()
+        {
+            return PetsWhere(p => p.sex == Sex.Female);
+        }
+
+        public IEnumerable<Pet> AllCatsOrDogs()
+        {
+            return PetsWhere(p => p.species == Species.Cat || p.species == Species.Dog);
+        }
+
+        public IEnumerable<Pet> AllPetsButNotMice()
+        {
+            return PetsWhere(p => p.species != Species.Mouse);
+        }
+
+        public IEnumerable<Pet> AllPetsBornAfter2010()
+        {
+            return PetsWhere(p => p.yearOfBirth> 2010);
+        }
+
+        public IEnumerable<Pet> AllPetsBornAfter2011OrRabbits()
+        {
+            return PetsWhere(p => p.yearOfBirth > 2011 || p.species == Species.Rabbit);
+        }
+
+        public IEnumerable<Pet> AllMaleDogs()
+        {
+            return PetsWhere(p => p.sex == Sex.Male && p.species == Species.Dog);
+        }
+
+        public IEnumerable<Pet> AllDogsBornAfter2010()
+        {
+            return PetsWhere(p => p.species == Species.Dog && p.yearOfBirth > 2010);
+        }
+        
+        private IEnumerable<Pet> PetsWhere(Func<Pet, bool> predicate)
+        {
+            foreach (var pet in _petsInTheStore)
+            {
+                if (predicate(pet))
+                {
+                    yield return pet;
+                }
+            }
         }
     }
 }
