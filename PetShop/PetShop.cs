0cc70449e962d21b5776d4388139ae44d17eef46
Julian Krabel <julian.krabel@ihsmarkit.com>
2022-11-22 09:09:03 +0100

    wydzielone metody wytworcze dla predykatów

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -28,7 +28,12 @@ namespace Training.DomainClasses
 
         public IEnumerable<Pet> AllCats()
         {
-            return _petsInTheStore.GetMatching(pet => pet.species == Species.Cat);
+            return _petsInTheStore.GetMatching(IsASpecie(Species.Cat));
+        }
+
+        private static Predicate<Pet> IsASpecie(Species species)
+        {
+            return pet => pet.species == species;
         }
 
         public IEnumerable<Pet> AllPetsSortedByName()
@@ -40,15 +45,18 @@ namespace Training.DomainClasses
 
         public IEnumerable<Pet> AllMice()
         {
-            return _petsInTheStore.GetMatching(pet => pet.species == Species.Mouse);
+            return _petsInTheStore.GetMatching(IsASpecie(Species.Mouse));
         }
 
         public IEnumerable<Pet> AllFemalePets()
         {
-            return _petsInTheStore.GetMatching(pet => pet.sex == Sex.Female);
+            return _petsInTheStore.GetMatching(IsFemale());
         }
 
-
+        private static Predicate<Pet> IsFemale()
+        {
+            return pet => pet.sex == Sex.Female;
+        }
 
         public IEnumerable<Pet> AllCatsOrDogs()
         {
@@ -57,12 +65,22 @@ namespace Training.DomainClasses
 
         public IEnumerable<Pet> AllPetsButNotMice()
         {
-            return _petsInTheStore.GetMatching(pet => pet.species != Species.Mouse);
+            return _petsInTheStore.GetMatching(IsNotASpecie(Species.Mouse));
+        }
+
+        private static Predicate<Pet> IsNotASpecie(Species species)
+        {
+            return pet => pet.species != species;
         }
 
         public IEnumerable<Pet> AllPetsBornAfter2010()
         {
-            return _petsInTheStore.GetMatching(pet => pet.yearOfBirth > 2010);
+            return _petsInTheStore.GetMatching(IsBornAfter(2010));
+        }
+
+        private static Predicate<Pet> IsBornAfter(int i)
+        {
+            return pet => pet.yearOfBirth > i;
         }
 
         public IEnumerable<Pet> AllDogsBornAfter2010()
