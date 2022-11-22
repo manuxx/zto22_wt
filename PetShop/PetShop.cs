4794922bc8df1e953128274b8657d89ee406f270
Szymon Knopp <knopp.s99@gmail.com>
2022-11-22 09:19:37 +0100

    move febricate methods to Pet

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -28,7 +28,7 @@ namespace Training.DomainClasses
 
         public IEnumerable<Pet> AllCats()
         {
-            return _petsInTheStore.GetMatching(IsSpecies(Species.Cat));
+            return _petsInTheStore.GetMatching(Pet.IsSpecies(Species.Cat));
         }
 
         public IEnumerable<Pet> AllPetsSortedByName()
@@ -40,12 +40,12 @@ namespace Training.DomainClasses
 
         public IEnumerable<Pet> AllMice()
         {
-            return _petsInTheStore.GetMatching(IsSpecies(Species.Mouse));
+            return _petsInTheStore.GetMatching(Pet.IsSpecies(Species.Mouse));
         }
 
         public IEnumerable<Pet> AllFemalePets()
         {
-            return _petsInTheStore.GetMatching(IsFemale());
+            return _petsInTheStore.GetMatching(Pet.IsFemale());
         }
 
         public IEnumerable<Pet> AllCatsOrDogs()
@@ -55,12 +55,12 @@ namespace Training.DomainClasses
 
         public IEnumerable<Pet> AllPetsButNotMice()
         {
-            return _petsInTheStore.GetMatching(IsNotSpecies(Species.Mouse));
+            return _petsInTheStore.GetMatching(Pet.IsNotSpecies(Species.Mouse));
         }
 
         public IEnumerable<Pet> AllPetsBornAfter2010()
         {
-            return _petsInTheStore.GetMatching(IsBornAfter(2010));
+            return _petsInTheStore.GetMatching(Pet.IsBornAfter(2010));
         }
 
         public IEnumerable<Pet> AllDogsBornAfter2010()
@@ -76,26 +76,6 @@ namespace Training.DomainClasses
         public IEnumerable<Pet> AllPetsBornAfter2011OrRabbits()
         {
             return _petsInTheStore.GetMatching(pet => pet.yearOfBirth > 2011 || pet.species == Species.Rabbit);
-        }
-
-        private static Predicate<Pet> IsSpecies(Species species)
-        {
-            return pet => pet.species == species;
-        }
-
-        private static Predicate<Pet> IsNotSpecies(Species species)
-        {
-            return pet => pet.species != species;
-        }
-
-        private static Predicate<Pet> IsFemale()
-        {
-            return pet => pet.sex == Sex.Female;
-        }
-
-        private static Predicate<Pet> IsBornAfter(int year)
-        {
-            return pet => pet.yearOfBirth > year;
         }
     }
 }
