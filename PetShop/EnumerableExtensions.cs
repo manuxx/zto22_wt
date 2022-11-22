f9058af2f05a596e847ef32eac42e4069083af86
manuxx <manus@pg.gda.pl>
2022-11-22 09:08:58 +0100

    wydzielone metodywytworcze dla predykatow

--- PetShop/EnumerableExtensions.cs
+++ PetShop/EnumerableExtensions.cs
@@ -12,7 +12,7 @@ static class EnumerableExtensions
         }
     }
 
-    public static IEnumerable<TItem> GetMatching<TItem>(this IEnumerable<TItem> items, Func<TItem, bool> condition)
+    public static IEnumerable<TItem> GetMatching<TItem>(this IEnumerable<TItem> items, Predicate<TItem> condition)
     {
         foreach (var item in items)
         {
--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -28,7 +28,17 @@ namespace Training.DomainClasses
 
         public IEnumerable<Pet> AllCats()
         {
-            return _petsInTheStore.GetMatching((pet => pet.species == Species.Cat));
+            return _petsInTheStore.GetMatching(IsASpeciesOf(Species.Cat));
+        }
+
+        private static Predicate<Pet> IsASpeciesOf(Species species)
+        {
+            return pet => pet.species == species;
+        }
+
+        public IEnumerable<Pet> AllPetsBornAfter2010()
+        {
+            return _petsInTheStore.GetMatching(IsBornAfter(2010));
         }
 
         public IEnumerable<Pet> AllPetsSortedByName()
@@ -40,12 +50,17 @@ namespace Training.DomainClasses
 
         public IEnumerable<Pet> AllMice()
         {
-            return _petsInTheStore.GetMatching((pet => pet.species == Species.Mouse));
+            return _petsInTheStore.GetMatching(IsASpeciesOf(Species.Mouse));
         }
 
         public IEnumerable<Pet> AllFemalePets()
         {
-            return _petsInTheStore.GetMatching((pet => pet.sex == Sex.Female));
+            return _petsInTheStore.GetMatching(IsFemale());
+        }
+
+        private static Predicate<Pet> IsFemale()
+        {
+            return pet => pet.sex == Sex.Female;
         }
 
         public IEnumerable<Pet> AllCatsOrDogs()
@@ -55,12 +70,18 @@ namespace Training.DomainClasses
 
         public IEnumerable<Pet> AllPetsButNotMice()
         {
-            return _petsInTheStore.GetMatching((pet => pet.species != Species.Mouse));
+            return _petsInTheStore.GetMatching(IsNotASpeciesOf(Species.Mouse));
         }
 
-        public IEnumerable<Pet> AllPetsBornAfter2010()
+        private static Predicate<Pet> IsNotASpeciesOf(Species species)
         {
-            return _petsInTheStore.GetMatching((pet => pet.yearOfBirth > 2010));
+            return pet => pet.species != species;
+        }
+
+
+        private static Predicate<Pet> IsBornAfter(int year)
+        {
+            return pet => pet.yearOfBirth > year;
         }
 
         public IEnumerable<Pet> AllDogsBornAfter2010()
