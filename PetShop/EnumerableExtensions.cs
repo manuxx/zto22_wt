350f94cb875632a70954bfeb740254a56ddc49bd
robeat1 <dzienisz.robert@gmail.com>
2022-11-22 09:51:34 +0100

    commit na koniec zajec 3

--- PetShop/EnumerableExtensions.cs
+++ PetShop/EnumerableExtensions.cs
@@ -28,22 +28,3 @@ static class EnumerableExtensions
         }
     }
 }
-
-public class AnonymousCriteria<TElement> : Criteria<TElement>
-{
-    private Predicate<TElement>_condition;
-    public AnonymousCriteria(Predicate<TElement> condition)
-    {
-        _condition = condition;
-    }
-
-    public bool IsSatisfiedBy(TElement pet)
-    {
-        return _condition(pet);
-    }
-}
-
-public interface Criteria<TElement>
-{
-    bool IsSatisfiedBy(TElement pet);
-}
--- PetShop/Pet.cs
+++ PetShop/Pet.cs
@@ -66,18 +66,4 @@ namespace Training.DomainClasses
             return new BornAfterCriteria(year);
         }
     }
-
-    public class BornAfterCriteria : Criteria<Pet>
-    {
-        private int _year;
-        public BornAfterCriteria(int year)
-        {
-            _year = year;
-        }
-
-        public bool IsSatisfiedBy(Pet item)
-        {
-            return item.yearOfBirth > _year;
-        }
-    }
 }
