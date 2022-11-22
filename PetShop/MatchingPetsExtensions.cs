17ad4d1a905a1229ab0bc89b74d3a93842a55f35
jusDol <justynadolega17@gmail.com>
2022-11-22 09:13:18 +0100

    l3

---
+++ PetShop/MatchingPetsExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Training.DomainClasses;
+
+    static class GetMatchingPetsExtensions
+    {
+        public static IEnumerable<TItem> GetMatchingPets<TItem>(this IEnumerable<TItem> items, Predicate<TItem> condition)
+        {
+            foreach (var item in items)
+            {
+                if (condition(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+
