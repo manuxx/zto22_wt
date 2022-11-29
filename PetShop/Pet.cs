e8b37218bcad4544532eb03172073a19857a2d74
robeat1 <dzienisz.robert@gmail.com>
2022-11-29 08:50:55 +0100

    dodane kryterium z negacja

--- PetShop/Pet.cs
+++ PetShop/Pet.cs
@@ -56,14 +56,29 @@ namespace Training.DomainClasses
             return new SexCriteria(Sex.Female);
         }
 
-        public static Predicate<Pet> IsNotASpeciesOf(Species species)
+        public static ICriteria<Pet> IsNotASpeciesOf(Species species)
         {
-            return pet => pet.species != species;
+            return new Negation<Pet>(IsASpeciesOf(species));
         }
 
         public static ICriteria<Pet> IsBornAfter(int year)
         {
             return new BornAfterCriteria(year);
+        }
+
+        private class Negation<TItem> : ICriteria<TItem>
+        {
+            private ICriteria<TItem> _criteriaToNegate;
+
+            public Negation(ICriteria<TItem> criteriaToNegate)
+            {
+                this._criteriaToNegate = criteriaToNegate;
+            }
+
+            public bool IsSatisfiedBy(TItem item)
+            {
+                return !_criteriaToNegate.IsSatisfiedBy(item);
+            }
         }
     }
 
