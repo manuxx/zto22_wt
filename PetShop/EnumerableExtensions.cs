9f55f444615445d6f447afd4462bc79de2249d62
Marianna <marianna.jucewicz@gmail.com>
2022-11-29 10:07:10 +0100

    cleanup

--- PetShop/EnumerableExtensions.cs
+++ PetShop/EnumerableExtensions.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Training.DomainClasses;
 
-static class EnumerableExtensions
+public static class EnumerableExtensions
 {
     public static IEnumerable<TItem> OneAtATime<TItem>(this IEnumerable<TItem> items)
     {
@@ -12,12 +12,7 @@ static class EnumerableExtensions
             yield return item;
         }
     }
-
-    public static IEnumerable<TItem> GetMatching<TItem>(this IEnumerable<TItem> items, Predicate<TItem> condition)
-    {
-        return items.GetMatching(new AnonymousCriteria<TItem>(condition));
-    }
-
+    
     public static IEnumerable<TItem> GetMatching<TItem>(this IEnumerable<TItem> items, ICriteria<TItem> criteria)
     {
         foreach (var item in items)
