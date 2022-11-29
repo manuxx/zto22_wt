4556ee60bb5dfa2801bd46c8ee0082c004874b8e
hevgan <adryanmaciej@gmail.com>
2022-11-29 09:43:34 +0100

    remove unused functions

--- PetShop/EnumerableExtensions.cs
+++ PetShop/EnumerableExtensions.cs
@@ -5,18 +5,6 @@ using Training.DomainClasses;
 
 static class EnumerableExtensions
 {
-    public static IEnumerable<TItem> OneAtATime<TItem>(this IEnumerable<TItem> items)
-    {
-        foreach (var item in items)
-        {
-            yield return item;
-        }
-    }
-
-    public static IEnumerable<TItem> GetMatching<TItem>(this IEnumerable<TItem> items, Predicate<TItem> condition)
-    {
-        return items.GetMatching(new AnonymousCriteria<TItem>(condition));
-    }
 
     public static IEnumerable<TItem> GetMatching<TItem>(this IEnumerable<TItem> items, ICriteria<TItem> criteria)
     {
