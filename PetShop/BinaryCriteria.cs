b6b05097723f4d3059cb4ff3ccec8e604e0307fa
robeat1 <dzienisz.robert@gmail.com>
2022-11-29 09:35:33 +0100

    binary crit

---
+++ PetShop/BinaryCriteria.cs
@@ -0,0 +1,16 @@
+﻿namespace Training.DomainClasses
+{
+    public abstract class BinaryCriteria<TItem> : ICriteria<TItem>
+    {
+        public ICriteria<Pet> _c1;
+        public ICriteria<Pet> _c2;
+
+        public BinaryCriteria(ICriteria<Pet> c1, ICriteria<Pet> c2)
+        {
+            _c1 = c1;
+            _c2 = c2;
+        }
+
+        public abstract bool IsSatisfiedBy(TItem item);
+    }
+}
