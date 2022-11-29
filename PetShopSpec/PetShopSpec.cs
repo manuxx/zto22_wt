38fc2eb5fa02b1627f199d864eeee9d8bb92f948
luckyluk07 <L.Nojman11@wp.pl>
2022-11-29 10:06:57 +0100

    Add changes in tests

--- PetShopSpec/PetShopSpec.cs
+++ PetShopSpec/PetShopSpec.cs
@@ -201,9 +201,15 @@ namespace Training.Specificaton
     {
         private It should_be_able_to_find_all_cats = () =>
         {
-            var foundPets = subject.AllCats();
+            ICriteria<Pet> criteria = Where(pet => pet.species).IsEqualTo(Species.Cat);
+            var foundPets = subject.AllPets().GetMatching(criteria);
             foundPets.ShouldContainOnly(cat_Tom, cat_Jinx);
         };
+
+        private static CriteriaBuilder Where(Func<Pet, Species> selector)
+        {
+            return new CriteriaBuilder(selector);
+        }
 
         private It should_be_able_to_find_all_mice = () =>
         {
@@ -251,6 +257,21 @@ namespace Training.Specificaton
         };
 
 
+    }
+
+    internal class CriteriaBuilder
+    {
+        private readonly Func<Pet, Species> _selector;
+
+        public CriteriaBuilder(Func<Pet, Species> selector)
+        {
+            _selector = selector;
+        }
+
+        public ICriteria<Pet> IsEqualTo(Species species)
+        {
+            return new AnonymousCriteria<Pet>(pet=> _selector(pet).Equals(species));
+        }
     }
 
 
