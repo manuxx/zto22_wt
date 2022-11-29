3754a7d96b2a73e48f945ca4d9221ebd19c2be0d
Marianna <marianna.jucewicz@gmail.com>
2022-11-29 09:26:01 +0100

    extract superclass from alternative and conjucnction

--- PetShop/CriteriaLogic.cs
+++ PetShop/CriteriaLogic.cs
@@ -13,35 +13,38 @@ namespace Training.DomainClasses
         }
     }
 
-    public class Alternative<TItem> : ICriteria<TItem>
+    public abstract class BinaryCriteria<TItem> : ICriteria<TItem>
     {
-        private readonly ICriteria<TItem> _criteria1;
-        private readonly ICriteria<TItem> _criteria2;
+        protected ICriteria<TItem> _criteria1;
+        protected ICriteria<TItem> _criteria2;
 
-        public Alternative(ICriteria<TItem> criteria1, ICriteria<TItem> criteria2)
+        public BinaryCriteria(ICriteria<TItem> criteria1, ICriteria<TItem> criteria2)
         {
             _criteria1 = criteria1;
             _criteria2 = criteria2;
         }
 
-        public bool IsSatisfiedBy(TItem item)
+        public abstract bool IsSatisfiedBy(TItem item);
+    }
+
+    public class Alternative<TItem> : BinaryCriteria<TItem>
+    {
+        public Alternative(ICriteria<TItem> criteria1, ICriteria<TItem> criteria2) : base(criteria1, criteria2)
+        {
+        }
+
+        public override bool IsSatisfiedBy(TItem item)
         {
             return (_criteria1.IsSatisfiedBy(item) || _criteria2.IsSatisfiedBy(item));
         }
     }
 
-    public class Conjunction<TItem> : ICriteria<TItem>
+    public class Conjunction<TItem> : BinaryCriteria<TItem>
     {
-        private readonly ICriteria<TItem> _criteria1;
-        private readonly ICriteria<TItem> _criteria2;
-
-        public Conjunction(ICriteria<TItem> criteria1, ICriteria<TItem> criteria2)
+        public Conjunction(ICriteria<TItem> criteria1, ICriteria<TItem> criteria2) : base(criteria1, criteria2)
         {
-            _criteria1 = criteria1;
-            _criteria2 = criteria2;
         }
-
-        public bool IsSatisfiedBy(TItem item)
+        public override bool IsSatisfiedBy(TItem item)
         {
             return (_criteria1.IsSatisfiedBy(item) && _criteria2.IsSatisfiedBy(item));
         }
