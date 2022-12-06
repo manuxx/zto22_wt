801eb34da2720b4b1c161ee0e5724993f2a87fbe
Julian Krabel <julian.krabel@ihsmarkit.com>
2022-12-06 09:05:04 +0100

    Shackowany IsGreaterThan

--- PetShopSpec/PetShopSpec.cs
+++ PetShopSpec/PetShopSpec.cs
@@ -199,7 +199,6 @@ namespace Training.Specificaton
 
     public class Where<TItem>
     {
-
         public static CriteriaBuilder<TItem,TProperty> HasAn<TProperty>(Func<TItem, TProperty> selector)
         {
             return new CriteriaBuilder<TItem,TProperty>(selector);
@@ -225,7 +224,7 @@ namespace Training.Specificaton
         private It should_be_able_to_find_all_female_pets = () =>
         {
             var criteria = Where<Pet>.HasAn(pet => pet.sex).IsEqualTo(Sex.Female);
-            var foundPets = subject.AllFemalePets();
+            var foundPets = subject.AllPets().GetMatching(criteria);
             foundPets.ShouldContainOnly(dog_Lassie, mouse_Dixie);
         };
         
@@ -243,7 +242,8 @@ namespace Training.Specificaton
        
         private It should_be_able_to_find_all_pets_born_after_2010 = () =>
         {
-            var foundPets = subject.AllPetsBornAfter2010();
+            var criteria = Where<Pet>.HasAn(pet => pet.yearOfBirth).IsGreaterThan(2010);
+            var foundPets = subject.AllPets().GetMatching(criteria);
             foundPets.ShouldContainOnly(dog_Pluto, rabbit_Fluffy, mouse_Dixie, mouse_Jerry);
         };
         private It should_be_able_to_find_all_young_dogs = () =>
@@ -274,9 +274,14 @@ namespace Training.Specificaton
             _selector = selector;
         }
 
-        public ICriteria<TItem> IsEqualTo(TProperty species)
+        public ICriteria<TItem> IsEqualTo(TProperty property)
         {
-            return new AnonymousCriteria<TItem>(pet=>_selector(pet).Equals(species));
+            return new AnonymousCriteria<TItem>(item=>_selector(item).Equals(property));
+        }
+
+        public ICriteria<TItem> IsGreaterThan(IComparable<TProperty> property)
+        {
+            return new AnonymousCriteria<TItem>(item => property.CompareTo(_selector(item)) < 0);
         }
     }
 
