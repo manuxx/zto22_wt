c9de5787009707e0d0a326b6cb53650bfef93b21
michal.matejuk <michal.matejuk@ihsmarkit.com>
2022-12-06 09:18:59 +0100

    wip

--- PetShopSpec/PetShopSpec.cs
+++ PetShopSpec/PetShopSpec.cs
@@ -235,7 +235,7 @@ namespace Training.Specificaton
         private It should_be_able_to_find_all_pets_born_after_2010 = () =>
         {
             var criteria = Where<Pet>.HasAn(p => p.yearOfBirth).IsGreaterThan(2010);
-            var foundPets = subject.AllPetsBornAfter2010();
+            var foundPets = subject.AllPets().GetMatching(criteria);
             foundPets.ShouldContainOnly(dog_Pluto, rabbit_Fluffy, mouse_Dixie, mouse_Jerry);
         };
         private It should_be_able_to_find_all_young_dogs = () =>
