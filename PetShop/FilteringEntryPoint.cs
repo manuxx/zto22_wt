27e8263cd1aa2185103828ff4a11fa783862f172
luckyluk07 <L.Nojman11@wp.pl>
2022-12-06 10:02:18 +0100

    Add second constructor

--- PetShop/FilteringEntryPoint.cs
+++ PetShop/FilteringEntryPoint.cs
@@ -5,15 +5,18 @@
         public readonly Func<TItem, TProperty> _selector;
         public bool _negationActive;
 
-        public FilteringEntryPoint(Func<TItem, TProperty> selector)
+        public FilteringEntryPoint(Func<TItem, TProperty> selector) : this(selector, false)
+        {
+        }
+
+        private FilteringEntryPoint(Func<TItem, TProperty> selector, bool negationActive)
         {
             _selector = selector;
-            _negationActive = false;
+            _negationActive = negationActive;
         }
 
         public FilteringEntryPoint<TItem, TProperty> Not()
         {
-            _negationActive = !_negationActive;
-            return this;
+            return new FilteringEntryPoint<TItem, TProperty>(_selector, !_negationActive);
         }
     }
