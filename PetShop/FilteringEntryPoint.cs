f131f1cc28a20dbbae900a95b4a2677237d30bc7
Jakub Brzozowski <brzozowski.jakub.andrzej@gmail.com>
2022-12-06 10:02:22 +0100

    make public constructor use private

--- PetShop/FilteringEntryPoint.cs
+++ PetShop/FilteringEntryPoint.cs
@@ -5,12 +5,8 @@
         public readonly Func<TItem, TProperty> _selector;
         public readonly bool _negation;
 
-        public FilteringEntryPoint(Func<TItem, TProperty> selector)
-        {
-            _selector = selector;
-            _negation = false;
-        }
-        
+        public FilteringEntryPoint(Func<TItem, TProperty> selector): this(selector, false) {}
+
         private FilteringEntryPoint(Func<TItem, TProperty> selector, bool negation)
         {
             _selector = selector;
