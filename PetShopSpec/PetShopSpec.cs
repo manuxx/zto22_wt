f785591c62337d3859e7bbf9543040f25f857f55
michal.matejuk <michal.matejuk@ihsmarkit.com>
2022-12-06 08:58:17 +0100

    liczbowe criteria

--- PetShopSpec/PetShopSpec.cs
+++ PetShopSpec/PetShopSpec.cs
@@ -234,6 +234,7 @@ namespace Training.Specificaton
        
         private It should_be_able_to_find_all_pets_born_after_2010 = () =>
         {
+            var criteria = Where<Pet>.HasAn(p => p.yearOfBirth).IsGreaterThan(2010);
             var foundPets = subject.AllPetsBornAfter2010();
             foundPets.ShouldContainOnly(dog_Pluto, rabbit_Fluffy, mouse_Dixie, mouse_Jerry);
         };
@@ -256,7 +257,7 @@ namespace Training.Specificaton
 
     }
 
-    internal class Where<TItem>
+    internal static class Where<TItem>
     {
         public static CriteriaBuilder<TItem,TProperty> HasAn<TProperty>(Func<TItem, TProperty> selector)
         {
@@ -276,6 +277,11 @@ namespace Training.Specificaton
         public ICriteria<TItem> IsEqualTo(TProperty value)
         {
             return new AnonymousCriteria<TItem>(item=>_selector(item).Equals(value));
+        }
+
+        public ICriteria<TItem> IsGreaterThan(IComparable<TProperty> value)
+        {
+            return new AnonymousCriteria<TItem>(item => value.CompareTo(_selector(item)) < 0);
         }
     }
 
