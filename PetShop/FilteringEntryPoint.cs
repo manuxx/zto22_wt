546803a9cfcf2d95d291d10eb614de5b242791df
@p1252778 <klaudia.slawecka@wp.pl>
2022-12-06 10:06:18 +0100

    changes

--- PetShop/FilteringEntryPoint.cs
+++ PetShop/FilteringEntryPoint.cs
@@ -3,9 +3,23 @@
     public class FilteringEntryPoint<TItem,TProperty> 
     {
         public readonly Func<TItem, TProperty> _selector;
+        public bool _negationActive = false;
 
         public FilteringEntryPoint(Func<TItem, TProperty> selector)
         {
             _selector = selector;
         }
-    }
+
+        public FilteringEntryPoint<TItem, TProperty> Not()
+        {
+            if(_negationActive == true)
+            {
+                _negationActive = false;
+            }
+            else
+            {
+                _negationActive = true;
+            }
+            return this;
+        }
+}
