ba4f243b4099402b15ea370b4b34d3aaf9ebe1a7
Szymon Knopp <knopp.s99@gmail.com>
2022-12-06 08:35:45 +0100

    Where

---
+++ PetShop/Where.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Training.DomainClasses;
+
+namespace PetShop
+{
+    public class Where<TItem, TField>
+    {
+        static CriteriaBuilder<TItem, TField> Has(Func<TItem, TField> selector)
+        {
+            return new CriteriaBuilder<TItem, TField>(selector);
+        }
+    }
+
+    internal class CriteriaBuilder<TItem, TField>
+    {
+        private readonly Func<TItem, TField> _selector;
+
+        public CriteriaBuilder(Func<TItem, TField> selector)
+        {
+            _selector = selector;
+        }
+
+        public ICriteria<Pet> IsEqualTo(Species species)
+        {
+            return new Pet.SpeciesCriteria(species);
+        }
+    }
+}
